f1f03606776a12469cc4c02f4d70a680c49cd626
Adam Ralph <adamralph@posteo.net>
2022-06-08 13:42:29 +1000

    create user created

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { CreateUserInput } from "../schema/user.schema";
+import { createUser } from "../service/user.service";
+import logger from "../utils/logger";
+
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
+  try {
+    const user = await createUser(req.body); // Call create user service
+    return user;
+  } catch (e: any) {
+    logger.error(e);
+    return res.status(409).send(e.message);
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import valiateResource from "./middleware/validateResource";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  app.post("/api/users", valiateResource(createUserHandler), createUserHandler);
 }
 
 export default routes;
