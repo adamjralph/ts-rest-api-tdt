69df398b406e03cc6573b8d6f143dbdb981d0410
Adam Ralph <adamralph@posteo.net>
2022-06-09 09:39:08 +1000

    create user working

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -2,13 +2,15 @@ import { Request, Response, NextFunction } from "express";
 import { AnyZodObject } from "zod";
 
 const validate =
-  (schema) => (req: Request, res: Response, next: NextFunction) => {
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
     try {
       schema.parse({
         body: req.body,
         query: req.query,
         params: req.params,
       });
+      next();
     } catch (e: any) {
       return res.status(400).send(e.errors);
     }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
 import valiateResource from "./middleware/validateResource";
+import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
-  app.post("/api/users", valiateResource(createUserHandler), createUserHandler);
+  app.post("/api/users", valiateResource(createUserSchema), createUserHandler);
 }
 
 export default routes;
