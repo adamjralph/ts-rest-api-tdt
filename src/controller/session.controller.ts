4f985db9957ffdcd0f263633275114e552508951
Adam Ralph <adamralph@posteo.net>
2022-06-10 11:59:19 +1000

    created user session

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import config from "config";
 import { createSession } from "../service/session.service";
 import { valdiatePassword } from "../service/user.service";
+import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // Validate the users' password
@@ -11,8 +13,20 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   }
 
   // create a session
-  const session = createSession(user._id, req.get("user-agent") || "");
+  const session = await createSession(user._id, req.get("user-agent") || "");
+
   // create an access token
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get("accessTokenTtl") } // 15 minutes
+  );
+
   // create a refresh token
+  const refreshToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get("refreshTokenTtl") } // 15 minutes
+  );
+
   // return
+  return res.send({ accessToken, refreshToken });
 }
